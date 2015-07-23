2c3e64b4007d22908ad6f344ca2d378f6173f0ca
Tanmay Garg <tanmaygarg@ymail.com>
2015-07-23 14:53:14 -0400

    added group.js route

---
+++ routes/groups.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var group = require('../controllers/group');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('just entered groups');
+});
+
+router.post('/add', group.add);
+router.post('/delete', group.delete);
+module.exports = router;
