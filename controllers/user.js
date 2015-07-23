89ae06517680491388523a7c96b9f7776f786ab2
Tanmay Garg <tanmaygarg@ymail.com>
2015-07-23 16:28:36 -0400

    user adding/delete group

--- controllers/user.js
+++ controllers/user.js
@@ -4,8 +4,21 @@ var Group = require("../models/group");
 var usercontroller = {};
 
 usercontroller.addgroup = function(req, res, next) {
-    var group = Group.findOne({ 'name': req.params.groupname }, function (err, data) {
-        data.users.add();
+    var group = Group.findOne({ 'name': req.body.groupname }, function (err, data) {
+        data.users.add(req.user._id);
+    });
+};
+
+usercontroller.removegroup = function (req, res, next) {
+    var group = Group.findOne({ 'name': req.body.groupname }, function (err, data) {
+        var index = data.users.indexOf(req.user._id);
+        if (index < 0) {
+            res.send("You are not a member of this group");
+        }
+        else {
+            data.users.splice(index, 1);
+            res.send("You have been removed from this group");
+        }
     });
 };
 
