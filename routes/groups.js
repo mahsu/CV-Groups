1873901a01b36960e2d640db5c199df5274278b5
Matthew Hsu <mhsu@commvault.com>
2015-07-23 15:34:35 -0400

    [fix] added some fields to user creation, cleanup

--- routes/groups.js
+++ routes/groups.js
@@ -1,12 +1,14 @@
+"use strict";
 var express = require('express');
 var router = express.Router();
 var group = require('../controllers/group');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('just entered groups');
+router.get('/', function (req, res, next) {
+    res.send('just entered groups');
 });
 
 router.post('/add', group.add);
 router.post('/delete', group.delete);
+
 module.exports = router;
