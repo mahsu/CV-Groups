99acddac9ff7dc48df3746f5950e96599b733c16
Tanmay Garg <tanmaygarg@ymail.com>
2015-07-23 15:59:01 -0400

    added group and user APIS

--- controllers/group.js
+++ controllers/group.js
@@ -1,19 +1,22 @@
 var Group = require("../models/group");
 
 var groupcontroller = {};
-groupcontroller.add = function register(req, res, next) {
+groupcontroller.add = function(req, res, next) {
     var group = new Group({
         //todo add more fields
         name: req.body.groupname,
         type: req.body.grouptype
     });
     group.save(function (err, data) {
-        if (err) console.log(err);
-        else console.log('Saved : ', data);
+        if (err) {
+            console.log(err);
+            res.send("Group creation failed");
+        }
+        else res.send("Group " + req.body.groupname + " successfully added ");
     });
 };
 
-groupcontroller.delete = function register(req, res, next) {
+groupcontroller.delete = function(req, res, next) {
     var group = Group.findOne({ 'name': req.body.groupname }).remove(function (err, data) {
         if (err) return handleError(err);
         else {
@@ -23,6 +26,22 @@ groupcontroller.delete = function register(req, res, next) {
     })
 };
 
+groupcontroller.showAll = function (req, res, next) {
+    var group = Group.find({}, function (err, data) {
+        res.send(data);
+    });
+};
 
+groupcontroller.findGroupByName = function (req, res, next) {
+    var group = Group.findOne({'name': req.params.groupname}, function (err, data) {
+        res.send(data);
+    });
+};
+
+groupcontroller.findGroupByType = function (req, res, next) {
+    var group = Group.find({ 'type': req.params.grouptype }, function (err, data) {
+        res.send(data);
+    });
+};
 
 module.exports = groupcontroller;
--- routes/groups.js
+++ routes/groups.js
@@ -3,9 +3,12 @@ var router = express.Router();
 var group = require('../controllers/group');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('just entered groups');
+router.get('/', function (req, res, next) {
+    res.send('just entered groups');
 });
+router.get('/showall', group.showAll);
+router.get('/show/:groupname', group.findGroupByName);
+router.get('/show/type/:grouptype', group.findGroupByType);
 
 router.post('/add', group.add);
 router.post('/delete', group.delete);
