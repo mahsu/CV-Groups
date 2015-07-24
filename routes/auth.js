337553aa784c945d0b2592b30643ca9820f48e23
Tanmay Garg <tanmaygarg@ymail.com>
2015-07-24 02:10:16 -0400

    added test result for auth

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,9 @@ router.post('/register', auth.register, function(req, res, next) {
 });
 
 router.get('/login', function(req, res, next) {});
-router.post('/login', passport.authenticate('local'), function(req, res, next) {});
+router.post('/login', passport.authenticate('local'), function (req, res, next) {
+    res.send("Login successful. ONLY FOR TESTING. DO REDIRECTS HERE");
+});
 
 router.get('/logout', auth.logout);
 
