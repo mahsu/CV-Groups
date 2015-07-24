3830781e4fb7f17f6fa8ab19e7fa97aca85627f9
Tanmay Garg <tanmaygarg@ymail.com>
2015-07-24 03:39:03 -0400

    removed post and comment schema from group model

--- models/group.js
+++ models/group.js
@@ -3,31 +3,14 @@ var mongoose = require('mongoose');
 
 //var User = require('./User');
 
-var commentSchema = new mongoose.Schema({
-    created_at: {type: Date, defualt: Date.now},
-    modified_at: {type: Date},
-    body: String,
-    author: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
-});
-
-var postSchema = new mongoose.Schema({
-    created_at: {type: Date, defualt: Date.now},
-    modified_at: {type: Date},
-    body: String,
-    author: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    tags: [String],
-    comments: [commentSchema],
-    upvoted_by: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-    downvoted_by: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
-});
 
 var grouptypes = 'classified carpool general finance'.split(' ');
 
 var groupSchema = new mongoose.Schema({
-    name: String,
+    name: { type: String, index: { unique: true } },
     type: { type: String, enum: grouptypes },
     users: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-    posts: [postSchema]
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }]
 });
 
 
