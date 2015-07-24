166b57b96669d6b1148f39b75f2e7a1e820c7fca
Matthew Hsu <mhsu@commvault.com>
2015-07-24 09:56:23 -0400

    continue resolving merge

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,7 +1,7 @@
 "use strict";
 var app = angular.module('cvgroups.controllers', []);
 
-app.controller('login.ctrl', ['$scope', function ($scope) {
+app.controller('login.ctrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
     $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
     'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
     'WY').split(' ').map(function (state) {
