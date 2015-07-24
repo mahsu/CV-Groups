1a83ada6454c885bc8bbcaa74cdc759a8afe6ebe
Matthew Hsu <mhsu@commvault.com>
2015-07-24 12:14:16 -0400

    [fix] login form validations

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,7 +1,7 @@
 "use strict";
-var app = angular.module('cvgroups.controllers', []);
+var app = angular.module('cvgroups.controllers', ['ngMessages']);
 
-app.controller('login.ctrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+app.controller('login.ctrl', ['$scope', '$http', '$location', '$mdToast', function ($scope, $http, $location, $mdToast) {
     $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
     'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
     'WY').split(' ').map(function (state) {
@@ -9,21 +9,34 @@ app.controller('login.ctrl', ['$scope', '$http', '$location', function ($scope, 
                 abbrev: state
             };
         });
-    $scope.login = function (user) {
+
+    $scope.login = function (user, valid) {
+        console.log(valid);
         $http.post('/api/auth/login', user)
             .success(function (data) {
                 $location.path('/landing');
             })
             .error(function (data, status, headers) {
-                //todo handle error
+                $mdToast.show(
+                    $mdToast.simple()
+                        .content('Invalid Username/Password.')
+                        .position("top left right")
+                        .hideDelay(2000)
+                );
                 console.log(data, status, headers);
             })
     };
 
-    $scope.register = function (user) {
+    $scope.register = function (user, valid) {
         geoCode(user.address, function (err, loc) {
             if (err) {
                 console.log("geocoding error");
+                $mdToast.show(
+                    $mdToast.simple()
+                        .content('Error in geocoding. Pleas check address.')
+                        .position("top left right")
+                        .hideDelay(2000)
+                );
             }
             else {
                 user.loc = loc;
@@ -36,6 +49,12 @@ app.controller('login.ctrl', ['$scope', '$http', '$location', function ($scope, 
                 .error(function (data, status, headers) {
                     //todo handle error
                     console.log(data, status, headers);
+                    $mdToast.show(
+                        $mdToast.simple()
+                            .content('Unable to complete user registration.')
+                            .position("top left right")
+                            .hideDelay(2000)
+                    );
                 });
             console.log(user);
         });
@@ -62,20 +81,20 @@ app.controller('landing.ctrl', ['$scope', '$http', '$mdSidenav', function ($scop
         })
         .error(function (data, status, headers, config) {
             console.log("failure");
-    });
+        });
 
     //Uncomment when logged in to get userGroups
     /*
-    $http.get('/api/users/viewGroup')
-        .success(function(data, status, headers, config) {
-            console.log("trying to get user groups");
-            $scope.userGroups = data;
-            console.log("get user groups succeeded, data in var userGroups");
-        })
-        .error(function (data, status, headers, config) {
-            console.log("failure");
-        });
-        */
+     $http.get('/api/users/viewGroup')
+     .success(function(data, status, headers, config) {
+     console.log("trying to get user groups");
+     $scope.userGroups = data;
+     console.log("get user groups succeeded, data in var userGroups");
+     })
+     .error(function (data, status, headers, config) {
+     console.log("failure");
+     });
+     */
 }]);
 
 function geoCode(address, callback) {
