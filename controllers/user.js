6fba35a1366f2edcd0117af32290192a3870c4ad
Tanmay Garg <tanmaygarg@ymail.com>
2015-07-24 02:09:58 -0400

    added user apis, adding to a group and removing from a group

--- controllers/user.js
+++ controllers/user.js
@@ -2,22 +2,58 @@ var User = require("../models/user");
 var Group = require("../models/group");
 
 var usercontroller = {};
+var resultJson = {};
+resultJson.status = 1;
+resultJson.res = "";
 
+//adding user to a particular group
 usercontroller.addgroup = function(req, res, next) {
     var group = Group.findOne({ 'name': req.body.groupname }, function (err, data) {
-        data.users.add(req.user._id);
+        var index = data.users.indexOf(req.user._id);
+        if (index == -1) {
+            data.users.push(req.user._id);
+            data.save(function (err) {
+                if (err) {
+                    resultJson.status = 0;
+                    resultJson.res = "Add to group failed";
+                    res.send(resultJson);
+                }
+                else {
+                    resultJson.res = "You have been added to this group";
+                    res.send(resultJson);
+                }
+            });
+        }
+        else {
+            resultJson.status = 0;
+            resultJson.res = "You already a member of this group";
+            res.send(resultJson);
+        }
     });
+    
 };
-
+//removing user from a particular group
 usercontroller.removegroup = function (req, res, next) {
     var group = Group.findOne({ 'name': req.body.groupname }, function (err, data) {
         var index = data.users.indexOf(req.user._id);
-        if (index < 0) {
-            res.send("You are not a member of this group");
+        if (index == -1) {
+            resultJson.status = 0;
+            resultJson.res = "You not a member of this group";
+            res.send(resultJson);
         }
         else {
             data.users.splice(index, 1);
-            res.send("You have been removed from this group");
+            data.save(function (err) {
+                if (err) {
+                    resultJson.status = 0;
+                    resultJson.res = "Removing from group failed";
+                    res.send(resultJson);
+                }
+                else {
+                    resultJson.res = "You have been removed from this group";
+                    res.send(resultJson);
+                }
+            });
         }
     });
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var user = require('../controllers/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('just entered users');
 });
+
+
+router.post('/addgroup', user.addgroup);
+router.post('/removegroup', user.removegroup);
+
 
 module.exports = router;
