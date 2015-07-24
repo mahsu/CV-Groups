5ab462a5ef99174477ce1cedeaa65b028b4ebda0
Matthew Hsu <mhsu@commvault.com>
2015-07-24 14:13:08 -0400

    [fix] add name to post and comment

--- models/post.js
+++ models/post.js
@@ -5,7 +5,13 @@ var postSchema = new mongoose.Schema({
     created_at: {type: Date, default: Date.now},
     modified_at: {type: Date},
     body: String,
-    author: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    author: {
+        _id: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+        name: {
+            first: String,
+            last: String
+        }
+    },
     tags: [String],
     comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}],
     upvoted_by: [{type: mongoose.Schema.Types.ObjectId, ref: 'User', index: { unique: true } }],
